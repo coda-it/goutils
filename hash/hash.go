bbb1b5ed3f02f704b2ac3ba145577acb26cd4c2b
Oskar Szura <oskar.szura@gmail.com>
2020-11-29 11:39:28 +0100

    CODA-127 - Move hash and session utils

---
+++ hash/hash.go
@@ -0,0 +1,15 @@
+package hash
+
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
+// HashString - transform string into hash
+func HashString(input string) string {
+	val := []byte(input)
+	h := sha1.New()
+	h.Write(val)
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
